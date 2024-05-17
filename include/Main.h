3f2850dc3733658298c0cb3725ef2e21e424d2c5
JlianSander <86343096+JlianSander@users.noreply.github.com>
2024-05-17 09:14:01 +0200

    update version number

--- include/Main.h
+++ include/Main.h
@@ -49,7 +49,7 @@ extern "C" {
 #include "../include/logic/Tests.h"
 
 constexpr auto SOLVERNAME = "ParallelSolver";
-constexpr auto VERSIONNUMBER = "1.8";
+constexpr auto VERSIONNUMBER = "1.11";
 constexpr auto NUM_CORES = 0;
 
 static int version_flag = 0;
