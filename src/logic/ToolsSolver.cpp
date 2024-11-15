a61d8ebebbc58d71a3270642518bbde4eea9f682
Julian <86343096+JlianSander@users.noreply.github.com>
2024-11-15 10:37:22 +0100

    cleaner call on ToolsSolver

--- src/logic/ToolsSolver.cpp
+++ src/logic/ToolsSolver.cpp
@@ -5,10 +5,8 @@
 void Tools_Solver::UpdateCertificate(SatSolver *solver, ArrayBitSet &active_args, std::__cxx11::list<uint32_t> &certificate_extension)
 {
 	list<uint32_t> calculated_extension = Decoding::get_set_from_solver(*solver, active_args);
-	list<uint32_t> new_proof = tools::ToolList::extend_list(certificate_extension, calculated_extension);
-	certificate_extension.clear();
+	Tools_Solver::UpdateCertificate(certificate_extension, calculated_extension);
 	calculated_extension.clear();
-	certificate_extension = new_proof;
 }
 
 /*===========================================================================================================================================================*/
