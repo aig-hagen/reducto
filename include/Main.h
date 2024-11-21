e4c8394652d0e63e7ae1089c0b7f8e7d9b1de0f3
Julian <86343096+JlianSander@users.noreply.github.com>
2024-11-21 14:16:46 +0100

    corrected version number

--- include/Main.h
+++ include/Main.h
@@ -52,7 +52,7 @@ extern "C" {
 #include "../include/logic/Enums.h"
 
 constexpr auto PROGRAM_NAME = "ParallelSolver";
-constexpr auto VERSIONNUMBER = "1.45";
+constexpr auto VERSIONNUMBER = "1.46";
 /// <summary>
 /// Number of cores requested to use during the computation of a solution.
 /// Keep in mind that the actual number of cores used depends on the 
