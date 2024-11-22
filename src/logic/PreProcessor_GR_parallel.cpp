99fad73c4345a9c3ba45b23f6ac69dfb1285875f
Julian <86343096+JlianSander@users.noreply.github.com>
2024-11-22 15:29:46 +0100

    Revert "reverted merge"
    This reverts commit 54065908489b1db54100e516f32ae089f4a8fa15.

--- src/logic/PreProcessor_GR_parallel.cpp
+++ src/logic/PreProcessor_GR_parallel.cpp
@@ -136,6 +136,106 @@ static ArrayBitSet calculate_cone_influence(AF &framework, uint32_t query) {
 /*===========================================================================================================================================================*/
 /*===========================================================================================================================================================*/
 
+
+static pre_proc_result reduce_by_grounded(AF &framework, ArrayBitSet &active_args, uint32_t query, bool break_accepted, bool break_rejected, ArrayBitSet &out_reduct, list<uint32_t> &out_gr_extension)
+{
+	omp_lock_t *lock_resources = (omp_lock_t *)malloc(sizeof * lock_resources);
+	if (lock_resources == NULL) {
+		printf("Memory allocation failed\n");
+		exit(1);
+	}
+	omp_init_lock(lock_resources);
+
+	pre_proc_result result = pre_proc_result::unknown;
+	// fill list with unattacked arguments
+	list<uint32_t> ls_unattacked_unprocessed;
+	vector<uint32_t> num_attacker;
+	num_attacker.resize(framework.num_args + 1);
+	//iterate through active arguments
+
+#pragma omp parallel for
+	for (int i = 0; i < active_args._array.size(); i++) {
+		//check if argument is unattacked
+		if (framework.attackers[active_args._array[i]].empty()) {
+			omp_set_lock(lock_resources);
+#pragma omp flush
+			ls_unattacked_unprocessed.push_back(active_args._array[i]);
+			out_gr_extension.push_back(active_args._array[i]);
+#pragma omp flush
+			omp_unset_lock(lock_resources);
+		}
+
+		num_attacker[active_args._array[i]] = framework.attackers[active_args._array[i]].size();
+	}
+
+	// init variable of current reduct
+	out_reduct = active_args;
+
+	//process list of unattacked arguments
+	for (list<uint32_t>::iterator mIter = ls_unattacked_unprocessed.begin(); mIter != ls_unattacked_unprocessed.end(); ++mIter) {
+		const auto &ua = *mIter;
+
+		//accept query if query is part of grounded extension
+		if (ua == query) {
+			if (break_accepted) {
+				return pre_proc_result::accepted;
+			}
+
+			result = pre_proc_result::accepted;
+		}
+
+		//reject query if it gets attacked by argument of grounded extension
+		if (framework.exists_attack(ua, query)) {
+			if (break_rejected) {
+				return pre_proc_result::rejected;
+			}
+
+			result = pre_proc_result::rejected;
+		}
+
+		//iterate through victims of the victims of ua
+#pragma omp parallel for
+		for (int i = 0; i < framework.victims[ua].size(); i++) {
+			uint32_t vua = framework.victims[ua][i];
+
+			if (!out_reduct._bitset[vua]) {
+				//only account victims that are still active
+				continue;
+			}
+
+			for (int j = 0; j < framework.victims[vua].size(); j++) {
+				uint32_t vvua = framework.victims[vua][j];
+
+				if (!out_reduct._bitset[vvua]) {
+					//only account victims of victims that are still active
+					continue;
+				}
+
+				//update number of attackers
+				omp_set_lock(lock_resources);
+#pragma omp flush
+				num_attacker[vvua]--;
+
+				//check if victim of victim is unattacked
+				if (num_attacker[vvua] == 0) {
+					ls_unattacked_unprocessed.push_back(vvua);
+					out_gr_extension.push_back(vvua);
+				}
+#pragma omp flush
+				omp_unset_lock(lock_resources);
+			}
+		}
+
+		//reduce active argument by unattacked argument + update current reduct
+		out_reduct = Reduct::get_reduct(out_reduct, framework, ua);
+	}
+
+	return result;
+}
+
+/*===========================================================================================================================================================*/
+/*===========================================================================================================================================================*/
+
 pre_proc_result PreProc_GR_parallel::process(AF &framework, uint32_t query, bool break_accepted, bool break_rejected, ArrayBitSet &out_reduct, list<uint32_t> &out_gr_extension)
 {
 	if (framework.self_attack[query])
