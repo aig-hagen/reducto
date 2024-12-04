d40ddd1922b70f14f4331d6ac82cd9120d450ea3
Julian <86343096+JlianSander@users.noreply.github.com>
2024-12-04 11:37:09 +0100

    adapting code of V1.49 to V1.41 part II

--- src/logic/Solver_DS_PR.cpp
+++ src/logic/Solver_DS_PR.cpp
@@ -3,21 +3,11 @@
 /*===========================================================================================================================================================*/
 /*===========================================================================================================================================================*/
 
-static void check_rejection(uint32_t query_argument, AF &framework, ArrayBitSet &active_args, bool &is_rejected, bool &is_terminated,
-	list<uint32_t> &extension_build, list<uint32_t> &certificate_extension, IPrioHeuristic &heuristic,
-	PriorityStackManager &prio_queue, bool isMain)
+void check_reduct(ArrayBitSet &reduct, AF &framework, uint32_t query_argument, bool &is_rejected, bool &is_terminated, std::__cxx11::list<uint32_t> &extension_build,
+	std::__cxx11::list<uint32_t> &certificate_extension, PriorityStackManager &prio_queue, IPrioHeuristic &heuristic, bool isMain)
 {
-	if (tools::ToolsOMP::check_termination(is_terminated, true)) return;
-	ArrayBitSet reduct = extension_build.empty() ? active_args.copy() : Reduct::get_reduct_set(active_args, framework, extension_build);
-	if (reduct._array.size() < 2)
-	{
-		// there is only 1 active argument, this has to be the argument to check, 
-		// if not then there should have been a rejection check earlier who did not work
-		return;
-	}
-	
 	uint64_t numVars = reduct._array.size();
-	SatSolver* solver = NULL;
+	SatSolver *solver = NULL;
 	solver = new SatSolver_cadical(numVars);
 	if (isMain) {
 		Encoding::add_clauses_nonempty_admissible_set(*solver, framework, reduct);
@@ -25,7 +15,7 @@ static void check_rejection(uint32_t query_argument, AF &framework, ArrayBitSet 
 	else {
 		Encoding::add_clauses_nonempty_complete_set(*solver, framework, reduct);
 	}
-	
+
 	bool continue_calculation = false;
 	list<uint32_t> calculated_set = Proc_DS_PR::calculate_rejecting_set(query_argument, framework, reduct, is_rejected, is_terminated,
 		*solver, continue_calculation, true);
@@ -35,7 +25,7 @@ static void check_rejection(uint32_t query_argument, AF &framework, ArrayBitSet 
 		prio_queue.try_insert_extension(query_argument, framework, &heuristic, new_extension, calculated_set);
 		new_extension.clear();
 
-		//iterate through initial sets
+		//iterate through sets in state
 		do {
 			Encoding::add_complement_clause(*solver, reduct);
 			calculated_set = Proc_DS_PR::calculate_rejecting_set(query_argument, framework, reduct, is_rejected, is_terminated,
@@ -43,10 +33,10 @@ static void check_rejection(uint32_t query_argument, AF &framework, ArrayBitSet 
 			list<uint32_t> new_extension_2 = tools::ToolList::extend_list(extension_build, calculated_set);
 			if (is_rejected) Tools_Solver::UpdateCertificate(certificate_extension, new_extension_2);
 			if (tools::ToolsOMP::check_termination(is_terminated, continue_calculation)) break;
-			
+
 			prio_queue.try_insert_extension(query_argument, framework, &heuristic, new_extension_2, calculated_set);
 			new_extension_2.clear();
-			
+
 		} while (!tools::ToolsOMP::check_termination(is_terminated, continue_calculation));
 	}
 
@@ -57,36 +47,50 @@ static void check_rejection(uint32_t query_argument, AF &framework, ArrayBitSet 
 /*===========================================================================================================================================================*/
 
 static void check_rejection(uint32_t query_argument, AF &framework, ArrayBitSet &active_args, bool &is_rejected, bool &is_terminated,
-	list<uint32_t> &extension_build, list<uint32_t> &output_extension, IPrioHeuristic &heuristic,
-	PriorityStackManager &prio_queue)
+	list<uint32_t> &extension_build, list<uint32_t> &certificate_extension, IPrioHeuristic &heuristic,
+	PriorityStackManager &prio_queue, bool isMain)
 {
-	if (check_termination(is_terminated, true)) return;
-	ArrayBitSet reduct = extension_build.empty() ? active_args.copy() : Reduct::get_reduct_set(active_args, framework, extension_build);
-	if (reduct._array.size() < 2)
-	{
-		// there is only 1 active argument, this has to be the argument to check, 
-		// if not then there should have been a rejection check earlier who did not work
-		return;
+	if (tools::ToolsOMP::check_termination(is_terminated, true)) return;
+	ArrayBitSet reduct;
+	if (extension_build.empty()) {
+		reduct = active_args.copy();
+		if (reduct._array.size() < 2)
+		{
+			// there is only 1 active argument, this has to be the argument to check, 
+			// if not then there should have been a rejection check earlier who did not work
+			return;
+		}
+
+		check_reduct(reduct, framework, query_argument, is_rejected, is_terminated, extension_build,
+			certificate_extension, prio_queue, heuristic, isMain);
 	}
+	else {
+		reduct = Reduct::get_reduct_set(active_args, framework, extension_build);
+		if (reduct._array.size() < 2)
+		{
+			// there is only 1 active argument, this has to be the argument to check, 
+			// if not then there should have been a rejection check earlier who did not work
+			return;
+		}
 
-	//Preprocess created reduct
-	ArrayBitSet preprocessed_reduct = ArrayBitSet();
-	pre_proc_result result_preProcessor = PreProc_DS_PR::process_reduct(framework, reduct, query_argument, preprocessed_reduct);
+		//Preprocess created reduct
+		ArrayBitSet preprocessed_reduct = ArrayBitSet();
+		pre_proc_result result_preProcessor = PreProc_GR::process_reduct(framework, reduct, query_argument, preprocessed_reduct);
+		switch (result_preProcessor) {
+		case accepted:
+			break;
 
-	switch (result_preProcessor) {
-	case accepted:
-		break;
+		case rejected:
+			tools::ToolsOMP::set_is_rejected(is_rejected, is_terminated);
+			Tools_Solver::UpdateCertificate(certificate_extension, extension_build);
+			break;
 
-	case rejected:
-		set_is_rejected(is_rejected, is_terminated);
-		output_extension = extension_build;
-		break;
-
-	default:
-		check_reduct(preprocessed_reduct, framework, query_argument, is_rejected, is_terminated, extension_build, output_extension, prio_queue, heuristic);
-		break;
+		default:
+			check_reduct(preprocessed_reduct, framework, query_argument, is_rejected, is_terminated, extension_build,
+				certificate_extension, prio_queue, heuristic, isMain);
+			break;
+		}
 	}
-	
 	return;
 }
 
