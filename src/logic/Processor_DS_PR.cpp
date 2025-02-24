86e0690daac248d510124f8b22055fa0256b7430
Julian <86343096+JlianSander@users.noreply.github.com>
2025-02-24 15:26:04 +0100

    bug

--- src/logic/Processor_DS_PR.cpp
+++ src/logic/Processor_DS_PR.cpp
@@ -31,30 +31,40 @@ list<uint32_t> Proc_DS_PR::calculate_rejecting_set(uint32_t query_argument, AF &
 		return list<uint32_t>();
 	}
 	else {
-		//check if set is PR, by checking if reduct has CO set
+		//check if found solution rejects query
 		list<uint32_t> calculated_set = get_set_from_solver(solver, active_args, query_argument, framework, is_rejected);
-		ArrayBitSet reduct = Reduct::get_reduct_set(active_args, framework, calculated_set);
-		SatSolver *solver_reduct = NULL;
-		solver_reduct = new SatSolver(reduct._array.size());
-		Encoding::add_clauses_nonempty_complete_set(*solver_reduct, framework, reduct);
-		if (!(*solver_reduct).solve())
-		{
-			// cannot calculate CO set in reduct, hence set used for reduction has to be a PR set
-			// since the PR set does not contain the query, it's a counter-example
-			is_rejected = true;
-		}
-		else {
-			//check if calculated CO extension rejects query
-			list<uint32_t> calculated_set_2 = get_set_from_solver(*solver_reduct, active_args, query_argument, framework, is_rejected);
-			//check if calculated CO extension contains query, if so then extend complement clause by the new extension
-			//since it is uninteresting to visit the combined extension once again
-			if (tools::ToolList::contains(calculated_set_2, query_argument)) {
-				list<uint32_t> calculated_set_tmp = tools::ToolList::extend_list(calculated_set, calculated_set_2);
-				calculated_set = calculated_set_tmp;
+		if (!is_rejected) {
+			//check if set is PR, by checking if reduct has CO set
+			ArrayBitSet reduct = Reduct::get_reduct_set(active_args, framework, calculated_set);
+			SatSolver *solver_reduct = NULL;
+			solver_reduct = new SatSolver(reduct._array.size());
+			Encoding::add_clauses_nonempty_complete_set(*solver_reduct, framework, reduct);
+			if (!(*solver_reduct).solve())
+			{
+				// cannot calculate CO set in reduct, hence set used for reduction has to be a PR set
+				// since the PR set does not contain the query, it's a counter-example
+				is_rejected = true;
 			}
-		}
+			else {
+				//check if calculated CO extension rejects query
+				list<uint32_t> calculated_set_2 = get_set_from_solver(*solver_reduct, active_args, query_argument, framework, is_rejected);
+				if (is_rejected) {
+					list<uint32_t> calculated_set_tmp = tools::ToolList::extend_list(calculated_set, calculated_set_2);
+					calculated_set = calculated_set_tmp;
+				}
+				else {
+					//check if calculated CO extension contains query, if so then extend complement clause by the new extension
+					//since it is uninteresting to visit the combined extension once again
+					if (tools::ToolList::contains(calculated_set_2, query_argument)) {
+						list<uint32_t> calculated_set_tmp = tools::ToolList::extend_list(calculated_set, calculated_set_2);
+						calculated_set = calculated_set_tmp;
+					}
+				}
+			}
 
-		delete solver_reduct;
+			delete solver_reduct;
+		}
+		
 		return calculated_set;
 	}
 }
