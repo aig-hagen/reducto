1d28231cba3f47201807b00cc773b3d5873beaf6
Julian <86343096+JlianSander@users.noreply.github.com>
2025-03-04 08:26:44 +0100

    docu

--- include/Main.h
+++ include/Main.h
@@ -51,7 +51,7 @@ extern "C" {
 #include "../include/logic/Enums.h"
 
 constexpr auto PROGRAM_NAME = "Reducto";
-constexpr auto VERSIONNUMBER = "2.07";
+constexpr auto VERSIONNUMBER = "2.08";
 
 /// <summary>
 /// Flags used for internal processing.
