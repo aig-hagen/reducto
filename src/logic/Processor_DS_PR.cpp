de4dd021f98209b6e85658f4c5aca3b17ae56d24
Julian <86343096+JlianSander@users.noreply.github.com>
2025-03-12 10:05:11 +0100

    bug

--- src/logic/Processor_DS_PR.cpp
+++ src/logic/Processor_DS_PR.cpp
@@ -22,7 +22,9 @@ list<uint32_t> Proc_DS_PR::calculate_rejecting_set(uint32_t query_argument, AF &
 		is_query_attacked = tools::Tools_ArgsSet::check_attack(query_argument, calculated_set, framework);
 		if (is_query_attacked) {
 			is_rejected = true;
+			return calculated_set;
 		}
+
 		//check if set is PR, by checking if reduct has CO set
 		ArrayBitSet reduct = Reduct::get_reduct_set(active_args, framework, calculated_set);
 		SatSolver *solver_reduct = NULL;
