d42b369d584954b57f487d335b31ba93e3a75ee7
Julian <86343096+JlianSander@users.noreply.github.com>
2025-04-14 11:33:14 +0200

    SE-PR conceptional reduction

--- src/logic/Solver_SE_PR.cpp
+++ src/logic/Solver_SE_PR.cpp
@@ -19,16 +19,14 @@ bool Solver_SE_PR::solve(AF &framework, ArrayBitSet &active_args, list<uint32_t>
 		list<uint32_t> calculated_extension = Decoding::get_set_from_solver(*solver, reduct);
 		tools::Tools_Solver::UpdateCertificate(out_certificate_extension, calculated_extension);
 		// reduce the current state by the calculated extension
-		ArrayBitSet new_reduct = Reduct::get_reduct_set(reduct, framework, calculated_extension);
-		reduct = new_reduct;
-		if (reduct._array.empty()) {
-			//no more arguments left, therefor preferred extension is found
-			break;
+		for (std::list<uint32_t>::iterator mIter = calculated_extension.begin(); mIter != calculated_extension.end(); ++mIter) {
+			solver->add_clause_short(Encoding::get_literal_accepted(*mIter, true), NULL);
+			solver->add_clause_short(Encoding::get_literal_rejected(framework, *mIter, false), NULL);
 		}
-		// calculate a complete extension in the new reduct
-		delete solver;
-		solver = new SatSolver();
-		Encoding::add_clauses_nonempty_complete_set(*solver, framework, reduct);
+
+		//ensure that solver does not find same solution again
+		Encoding::add_complement_clause(*solver, active_args);
+
 		has_solution = (*solver).solve();
 		//if has_solution == false, then no more complete set can be calculated, therefor extension found so far cannot be extended, and is therefor preferred
 		calculated_extension.clear();
