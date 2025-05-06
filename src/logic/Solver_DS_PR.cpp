8602aa9286edb83fa39db49ad50323536b02ed4e
Julian <86343096+JlianSander@users.noreply.github.com>
2025-05-06 10:13:00 +0200

    removed completion of PR extensions

--- src/logic/Solver_DS_PR.cpp
+++ src/logic/Solver_DS_PR.cpp
@@ -38,30 +38,6 @@ static bool search_complete_sets_in_state(AF &framework, ArrayBitSet &reduct, ui
 /*===========================================================================================================================================================*/
 /*===========================================================================================================================================================*/
 
-/// <summary>
-/// This method completes the preferred extension used as an certificate
-/// </summary>
-static void complete_certificate(AF &framework, list<uint32_t> &out_certificate_extension) {
-	// get the original state of the framework
-	ArrayBitSet original_active_args = framework.create_active_arguments();
-	// reduce the original state by the extension found so far
-	ArrayBitSet reduct = out_certificate_extension.empty() ? original_active_args : Reduct::get_reduct_set(original_active_args, framework, out_certificate_extension);
-	// abort if no active argument is left
-	if (reduct._array.empty()) {
-		return;
-	}
-	// use the algorithm for the SE-PR problem to compute a preferred extension in the reduct
-	list<uint32_t> extension_outside_certificate;
-	bool has_extension_outside_certificate = Solver_SE_PR::solve(framework, reduct, extension_outside_certificate);
-	// update the certificate if such a set was found
-	if (has_extension_outside_certificate) {
-		tools::Tools_Solver::UpdateCertificate(out_certificate_extension, extension_outside_certificate);
-	}
-}
-
-/*===========================================================================================================================================================*/
-/*===========================================================================================================================================================*/
-
 bool Solver_DS_PR::solve(uint32_t query_argument, AF &framework, list<uint32_t> &out_certificate_extension)
 {
 	// initialize variables
@@ -79,21 +55,11 @@ bool Solver_DS_PR::solve(uint32_t query_argument, AF &framework, list<uint32_t> 
 		return true;
 
 	case rejected:
-		// complete certificate if it does not attack the query
-		if (!framework.check_attack(query_argument, out_certificate_extension, framework)) {
-			complete_certificate(framework, out_certificate_extension);
-		}
 		return false;
 
 	default:
 		bool is_query_attacked = false;
 		bool is_skeptically_accepted = search_complete_sets_in_state(framework, active_args_in_coi, query_argument, out_certificate_extension, coi, is_query_attacked);
-
-		//if skeptical acceptance of query got rejected, but query is not attacked by certificate, then extend certificate to get complete PR extension in original AF
-		if (!is_skeptically_accepted && !is_query_attacked) {
-			complete_certificate(framework, out_certificate_extension);
-			return is_skeptically_accepted;
-		}
 
 		return is_skeptically_accepted;
 	}
