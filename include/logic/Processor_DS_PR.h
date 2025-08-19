19204c5aa978134bb89ad9ef3229509aec1029fa
JlianSander <julian.sander@mailbox.org>
2025-08-19 12:48:03 +0000

    improved certificate completion

--- include/logic/Processor_DS_PR.h
+++ include/logic/Processor_DS_PR.h
@@ -29,12 +29,11 @@ public:
 	/// <param name="active_args">The arguments that are active in the current state of the framework.</param>
 	/// <param name="out_is_rejected">[Output-Parameter] Flag used to signal that the argument is rejected.</param>
 	/// <param name="out_is_query_attacked">[Output-Parameter] Flag used to signal if the calculated set of arguments attacks the query argument.</param>
-	/// <param name="out_is_complete_pr">[Output-Parameter] Flag used to signal if the calculated set of arguments is a complete preferred extension.</param>
 	/// <param name="solver">The solver used to calculate sets for this state of the framework.</param>
 	/// <param name="out_has_solution_without_query">[Output-Parameter] Flag used to signal if the solver could calculate a solution.</param>
 	/// <param name="is_first_iteration">Flag indicating if this iteration to calculate a set is the first in the specified state of the framework.</param>
 	/// <returns>An non emtpy set, if such a set can be computed. Empty list otherwise.</returns>
-	static list<uint32_t> calculate_rejecting_set(uint32_t query_argument, AF &framework, ArrayBitSet &active_args, bool &out_is_rejected, bool &out_is_query_attacked, bool &out_is_complete_pr,
+	static list<uint32_t> calculate_rejecting_set(uint32_t query_argument, AF &framework, ArrayBitSet &active_args, bool &out_is_rejected, bool &out_is_query_attacked, 
 		SatSolver &solver, bool &out_has_solution_without_query, bool is_first_iteration);
 };
 #endif
